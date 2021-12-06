ac165e64b854b0f0f0d73e118366fab7f53596c3
unknown <debnath.p.slg@gmail.com>
2021-12-06 23:49:41 +0530

    202112062349

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RestaurantDashComponent } from './restaurant-dash/restaurant-dash.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [AppComponent, RestaurantDashComponent],
+  declarations: [AppComponent, RestaurantDashComponent, LoginComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
--- src/app/restaurant-dash/restaurant-dash.component.ts
+++ src/app/restaurant-dash/restaurant-dash.component.ts
@@ -10,16 +10,17 @@ import { RestaurantData } from './restaurant.model';
 })
 export class RestaurantDashComponent implements OnInit {
   formValue!: FormGroup;
-
   restaurantModelObj: RestaurantData = new RestaurantData();
   allRestaurantData: any;
+  showAddBtn!: boolean;
+  showEditBtn!: boolean;
 
   constructor(private formBuilder: FormBuilder, private api: ApiService) {}
 
   ngOnInit(): void {
     this.formValue = this.formBuilder.group({
       name: [''],
-      emailId: [''],
+      email: [''],
       mobile: [''],
       address: [''],
       services: [''],
@@ -28,7 +29,22 @@ export class RestaurantDashComponent implements OnInit {
     this.getAllData();
   }
 
+  clickAddResto() {
+    this.formValue.reset();
+    this.showAddBtn = true;
+    this.showEditBtn = false;
+  }
+
+  clickEditResto() {
+    this.formValue.reset();
+    this.showAddBtn = false;
+    this.showEditBtn = true;
+  }
+
   addResto() {
+    this.showAddBtn = true;
+    this.showEditBtn = false;
+
     this.restaurantModelObj.name = this.formValue.value.name;
     this.restaurantModelObj.email = this.formValue.value.email;
     this.restaurantModelObj.mobile = this.formValue.value.mobile;
@@ -37,6 +53,12 @@ export class RestaurantDashComponent implements OnInit {
 
     this.api.postRestaurant(this.restaurantModelObj).subscribe((res) => {
       alert('Restaurant record added successfully...');
+
+      let ref = document.getElementById('clear');
+      ref?.click();
+
+      this.formValue.reset;
+      this.getAllData();
     }),
       (err: any) => {
         alert('Something went wrong !!!');
@@ -47,5 +69,47 @@ export class RestaurantDashComponent implements OnInit {
     this.api.getRestaurant().subscribe((res) => {
       this.allRestaurantData = res;
     });
+  }
+
+  deleteResto(data: any) {
+    if (confirm('Do you want to delete ' + data.name)) {
+      this.api.deleteRestaurant(data.id).subscribe((res) => {
+        alert(data.name + ' deleted successfully');
+
+        this.getAllData();
+      });
+    }
+  }
+
+  onEditResto(data: any) {
+    this.showAddBtn = false;
+    this.showEditBtn = true;
+
+    this.restaurantModelObj.id = data.id;
+    this.formValue.controls['name'].setValue(data.name);
+    this.formValue.controls['email'].setValue(data.email);
+    this.formValue.controls['mobile'].setValue(data.mobile);
+    this.formValue.controls['address'].setValue(data.address);
+    this.formValue.controls['services'].setValue(data.services);
+  }
+
+  updateResto() {
+    this.restaurantModelObj.name = this.formValue.value.name;
+    this.restaurantModelObj.email = this.formValue.value.email;
+    this.restaurantModelObj.mobile = this.formValue.value.mobile;
+    this.restaurantModelObj.address = this.formValue.value.address;
+    this.restaurantModelObj.services = this.formValue.value.services;
+
+    this.api
+      .updateRestaurant(this.restaurantModelObj, this.restaurantModelObj.id)
+      .subscribe((res) => {
+        alert(this.restaurantModelObj.name + ' updated succeddfully...');
+
+        let ref = document.getElementById('clear');
+        ref?.click();
+
+        this.formValue.reset;
+        this.getAllData();
+      });
   }
 }
