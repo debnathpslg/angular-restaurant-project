a101eb30642a816e219d6251f5da121541fd81e2
unknown <debnath.p.slg@gmail.com>
2021-12-08 02:11:36 +0530

    202112080211

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,49 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private _http: HttpClient,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: [''],
+      password: [''],
+    });
   }
 
+  submitLogin() {
+    this._http.get<any>('http://localhost:3000/users').subscribe((res) => {
+      const userFound = res.find((a: any) => {
+        return (
+          a.email === this.loginForm.value.email &&
+          a.password === this.loginForm.value.password
+        );
+      });
+
+      if (userFound) {
+        alert('User logged in successfully');
+        this.loginForm.reset();
+        this.router.navigate(['/resto-dash']);
+      } else {
+        alert('Wrong credentila. Please check and login again...');
+        this.loginForm.reset();
+      }
+    }),
+      (err: any) => {
+        alert('Something went wrong while loggin user !!!');
+      };
+  }
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,15 +1,41 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+  styleUrls: ['./signup.component.css'],
 })
 export class SignupComponent implements OnInit {
+  signupForm!: FormGroup;
 
-  constructor() { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private _http: HttpClient,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    this.signupForm = this.formBuilder.group({
+      userName: [''],
+      email: [''],
+      password: [''],
+    });
   }
 
+  signUp() {
+    this._http
+      .post<any>('http://localhost:3000/users', this.signupForm.value)
+      .subscribe((res) => {
+        alert('Registration Successful...');
+
+        this.signupForm.reset();
+        this.router.navigate(['/login']);
+      }),
+      (err: any) => {
+        alert('Something went wrong while registering user !!!');
+      };
+  }
 }
